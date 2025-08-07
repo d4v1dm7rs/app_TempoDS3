6599da537a784be849a4de0ee3db49fd32668d7a
d4v1dm7rs <ds093461@gmail.com>
2025-08-07 01:39:15 -0300

    primeiro passo concluido

---
+++ views/home.tsx
@@ -0,0 +1,17 @@
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default function Home() {
+    return (
+        <View>
+            <Text>Seja bem vindo!</Text>
+
+            <TouchableOpacity>
+                <Text>Itanhaém</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styels = StyleSheet.create({
+    
+});
